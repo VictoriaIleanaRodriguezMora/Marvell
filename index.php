4ef2099f406a5f2528333f185f6c99c2e3d5b696
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-03-11 13:50:51 -0300

    Icono

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://unpkg.com/@popperjs/core@2">
-
+  <link rel="shortcut icon" type="Imagenes/principal/icono.ico" href="/favicon.ico">
 </head>
 
 <body>
