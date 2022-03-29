56552be14ace7c732ff067c7785e5e399121d62d
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-03-29 20:29:43 -0300

    Cambios quizz.js

--- js/quizz.js
+++ js/quizz.js
@@ -2,20 +2,17 @@
 let datoUsuario = parseInt(prompt("¿Por quien consigue Clint la gema del Alma?. \n Ingresá el nro correspondiente. \n 1. Viuda Negra \n 2.Bruja Escarlata \n 3.Gamora \n 4.Carol Danvers \n 0. Para salir"))
 let contadorAcertadas = 0
 let contadorDesacertadas = 0
-let cantidadPreguntas = 2
 
 function switchUser() {
     switch (datoUsuario) {
         case 1:
             contadorAcertadas++
-            cantidadPreguntas--
             break;
         case 0:
             alert(`Has acertado ${contadorAcertadas}, y desacertado ${contadorDesacertadas}`)
             break;
         default:
             contadorDesacertadas++
-            cantidadPreguntas--
             break;
     }
 }
@@ -26,7 +23,6 @@ function jugar() {
         switchUser()
         datoUsuario = parseInt(prompt("¿Qué personas tiene un premio por intrepretar en live action más veces a un superhéroe de marvel? \n 1. Hugh JackMan & Patrick Stewart \n 2. Hugh JackMan & Robert D. Junior\n 3. Patrick Stewart & Robert D. Junior \n 0. Para salir"))
         switchUser()
-
 
         alert(`Has acertado ${contadorAcertadas}, y desacertado ${contadorDesacertadas}.`)
         break
