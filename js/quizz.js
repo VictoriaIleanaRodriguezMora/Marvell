ae0c0c908e3bef4e7fbccf22f610a117ea8ca340
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-03-31 10:17:39 -0300

    Pase de quizz2.js a quizz.js

--- js/quizz.js
+++ js/quizz.js
@@ -1,15 +1,55 @@
-// let jugarSN = parseInt(prompt("Quieres jugar?. \n 1. SÍ \n 2. No"))
-let datoUsuario = parseInt(prompt("¿Por quien consigue Clint la gema del Alma?. \n Ingresá el nro correspondiente. \n 1. Viuda Negra \n 2. Bruja Escarlata \n 3. Gamora \n 4. Carol Danvers \n 0. Para salir"))
+let elijeJuego = parseInt(prompt("Sobre qué tematica queres jugar? Ingresá el nro. \n 1. UCM \n 2. X-men"))
+let datoUsuario
 let contadorAcertadas = 0
 let contadorDesacertadas = 0
 
-function switchUser() {
+function elegirJuego() {
+    switch (elijeJuego) {
+        case 1:
+            preguntasUCM()
+            break;
+        case 2:
+            preguntasXMEN()
+            break;
+        case 3:
+            todo()
+            break;
+        case 0:
+            break;
+    }
+}
+elegirJuego()
+
+function cerrarJuego() {
+    elijeJuego = parseInt(prompt("Queres jugar de nuevo? \n 1. Sí \n 2. No "))
+    switch (elijeJuego) {
+        case 1:
+            elijeJuego = parseInt(prompt("Sobre qué tematica queres jugar? Ingresá el nro. \n 1. UCM \n 2. X-men \n 3. TODO"))
+            elegirJuego()
+            break;
+        case 2:
+            break;
+    }
+}
+
+function jugarDeNuevo() {
+    alertScore()
+    contadorAcertadas = 0
+    contadorDesacertadas = 0
+    cerrarJuego()
+}
+
+function alertScore() {
+    alert(`Has acertado ${contadorAcertadas}, y desacertado ${contadorDesacertadas}.`)
+}
+
+function contadorDePuntos() {
+
     switch (datoUsuario) {
         case 1:
             contadorAcertadas++
             break;
         case 0:
-            alert(`Has acertado ${contadorAcertadas}, y desacertado ${contadorDesacertadas}`)
             break;
         default:
             contadorDesacertadas++
@@ -17,19 +57,29 @@ function switchUser() {
     }
 }
 
-function jugar() {
-    while (datoUsuario != 0) {
-        switchUser()
-        console.log(contadorAcertadas)
-        datoUsuario = parseInt(prompt("Que raza es aliada de Loki en Avengers? \n Ingresá el nro correspondiente. \n 1. Chitauri \n 2. Klyntar \n 3. Kronans \n 0. Para salir"))
-        console.log(contadorAcertadas)
-        switchUser()
-        datoUsuario = parseInt(prompt("¿Qué personas tiene un premio por intrepretar en live action más veces a un superhéroe de marvel? \n 1. Hugh JackMan & Patrick Stewart \n 2. Hugh JackMan & Robert D. Junior\n 3. Patrick Stewart & Robert D. Junior \n 0. Para salir"))
-        console.log(contadorAcertadas)
-        switchUser()
+function preguntasUCM() {
 
-        alert(`Has acertado ${contadorAcertadas}, y desacertado ${contadorDesacertadas}.`)
-        break
-    }
+    datoUsuario = parseInt(prompt("Que raza es aliada de Loki en Avengers? \n Ingresá el nro correspondiente. \n 1. Chitauri \n 2. Klyntar \n 3. Kronans \n 0. Para salir"))
+    contadorDePuntos()
+    datoUsuario = parseInt(prompt("¿Por quien consigue Clint la gema del Alma?. \n Ingresá el nro correspondiente. \n 1. Viuda Negra \n 2. Bruja Escarlata \n 3. Gamora \n 4. Carol Danvers \n 0. Para salir"))
+    contadorDePuntos()
+    jugarDeNuevo()
+
 }
-jugar()
+
+function preguntasXMEN() {
+
+    datoUsuario = parseInt(prompt("¿Qué personas tiene un premio por intrepretar en live action más veces a un superhéroe de marvel? \n 1. Hugh JackMan & Patrick Stewart \n 2. Hugh JackMan & Robert D. Junior\n 3. Patrick Stewart & Robert D. Junior \n 0. Para salir"))
+    contadorDePuntos()
+    datoUsuario = parseInt(prompt("¿Por quien consigue Clint la gema del Alma?. \n Ingresá el nro correspondiente. \n 1. Viuda Negra \n 2. Bruja Escarlata \n 3. Gamora \n 4. Carol Danvers \n 0. Para salir"))
+    contadorDePuntos()
+    jugarDeNuevo()
+}
+
+
+// function todo() {
+//     preguntasXMEN()
+//     preguntasUCM()
+// }
+
+// todo()
