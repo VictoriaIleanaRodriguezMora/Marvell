ec63b8ab4282e6bd823d6d8f98fce5037f5b9788
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-04-02 19:55:27 -0300

    Version funcional de class Pregunta

--- js/quizz.js
+++ js/quizz.js
@@ -2,9 +2,114 @@ let datoUsuario
 let contadorAcertadas = 0
 let contadorDesacertadas = 0
 
+
+
+class Pregunta {
+    constructor(pregunta, opciones, datoDelUsuario) {
+        this.pregunta = pregunta;
+        this.opciones = opciones;
+        this.datoDelUsuario = datoDelUsuario;
+
+    }
+    mostrarPregunta() {
+        this.datoDelUsuario = parseInt(prompt(this.pregunta + this.opciones))
+    }
+    contadorDePuntos() {
+        switch (this.datoDelUsuario) {
+            case 1:
+                contadorAcertadas++
+                break;
+            case 2:
+                contadorDesacertadas++
+                break;
+            case 3:
+                contadorDesacertadas++
+                break;
+            case 4:
+                contadorDesacertadas++
+                break;
+            case 0:
+                break;
+            default:
+                break;
+        }
+    }
+    alertScore() {
+        alert(`Has acertado ${contadorAcertadas}, y desacertado ${contadorDesacertadas}.`)
+    }
+    jugarDeNuevo() {
+        this.alertScore()
+        contadorAcertadas = 0
+        contadorDesacertadas = 0
+        elijeJuego = parseInt(prompt("Queres jugar de nuevo? \n 1. Sí \n 2. No "))
+        switch (elijeJuego) {
+            case 1:
+                elijeJuego = parseInt(prompt("Sobre qué tematica queres jugar? Ingresá el nro. \n 1. UCM \n 2. X-men \n 3. TODO"))
+                elegirJuego()
+                break;
+            case 2:
+                break;
+        }
+    }
+
+}
+
+function preguntasUCM() {
+
+    let preguntaUCM1 = new Pregunta("Que raza es aliada de Loki en Avengers? Ingresá el numero correspondiente.",
+        "\n1. Chitauri \n 2. Klyntar \n 3. Kronans \n 0. Para salir", datoUsuario)
+    preguntaUCM1.mostrarPregunta()
+    preguntaUCM1.contadorDePuntos()
+
+    let preguntaUCM2 = new Pregunta("¿Por quien consigue Clint la gema del Alma? \n Ingresá el nro correspondiente.",
+        "\n 1. Viuda Negra \n 2. Bruja Escarlata \n 3. Gamora \n 4. Carol Danvers \n 0. Para salir", datoUsuario)
+    preguntaUCM2.mostrarPregunta()
+    preguntaUCM2.contadorDePuntos()
+
+    preguntaUCM2.jugarDeNuevo()
+}
+
+function preguntasXMEN() {
+    let preguntaXMEN1 = new Pregunta("¿Qué personas tiene un premio por intrepretar en live action más veces a un superhéroe de marvel? \n Ingresá el numero correspondiente.",
+        "\n 1. Hugh JackMan & Patrick Stewart \n 2. Hugh JackMan & Robert D. Junior\n 3. Patrick Stewart & Robert D. Junior \n 0. Para salir", datoUsuario)
+    preguntaXMEN1.mostrarPregunta()
+    preguntaXMEN1.contadorDePuntos()
+
+    let preguntaXMEN2 = new Pregunta("¿En que pelicula Wolverine viaja al pasado? \n Ingresá el nro correspondiente.",
+        "\n 1. X-men Dias del Futuro Pasado \n 2. X-men 0rigenes \n 3. X-men 2 \n 0. Para salir", datoUsuario)
+    preguntaXMEN2.mostrarPregunta()
+    preguntaXMEN2.contadorDePuntos()
+
+    let preguntaXMEN3 = new Pregunta("¿Como se llama la nave de los X-men? \n Ingresá el nro correspondiente.",
+        "\n 1. X-jet \n 2. X-ship \n 3. X-moon", datoUsuario)
+    preguntaXMEN3.mostrarPregunta()
+    preguntaXMEN3.contadorDePuntos()
+
+    let preguntaXMEN4 = new Pregunta("¿Que accidente tiene Charles Xavier que lo deja invalido? \n Ingresá el nro correspondiente.",
+        "\n 1. Tiro en la espina dorsal \n 2. Accidente de auto \n 3. Golpe en la cabeza", datoUsuario)
+    preguntaXMEN4.mostrarPregunta()
+    preguntaXMEN4.contadorDePuntos()
+
+
+    let preguntaXMEN5 = new Pregunta("¿En donde ocurre 'La Batalla Final' donde Logan tiene que matar a Phoenix? \n Ingresá el nro correspondiente.",
+        "\n 1. Alcatraz \n 2. Genosha \n 3. Canada", datoUsuario)
+    preguntaXMEN5.mostrarPregunta()
+    preguntaXMEN5.contadorDePuntos()
+
+    let preguntaXMEN6 = new Pregunta("¿Donde nació Lobezno? \n Ingresá el nro correspondiente.",
+        "\n 1. Canada \n 2. EEUU \n 3. Inglaterra", datoUsuario)
+    preguntaXMEN6.mostrarPregunta()
+    preguntaXMEN6.contadorDePuntos()
+
+    let preguntaXMEN7 = new Pregunta("¿Cual es el verdadero nombre de Logan? \n Ingresá el nro correspondiente.",
+        "\n 1. Shimmy \n 2. James \n 3. David", datoUsuario)
+    preguntaXMEN7.mostrarPregunta()
+    preguntaXMEN7.contadorDePuntos()
+
+    preguntaXMEN7.jugarDeNuevo()
+}
+
 let elijeJuego = parseInt(prompt("Sobre qué tematica queres jugar? Ingresá el nro. \n 1. UCM \n 2. X-men"))
-    //elijeJuego inicialmente pude valer 1 || 2
-    //Pero despues puede tomar 4 valores 1, 2, 3 y 0
 
 function elegirJuego() {
     switch (elijeJuego) {
@@ -24,87 +129,3 @@ function elegirJuego() {
 }
 // Si no se ejecuta, no inicia el juego
 elegirJuego()
-
-// Se ejecuta una vez finalizadas las preguntas. Y reestablece los contadores, para jugar de nuevo.
-//Es de callback
-
-function jugarDeNuevo() {
-    alertScore()
-    contadorAcertadas = 0
-    contadorDesacertadas = 0
-    elijeJuego = parseInt(prompt("Queres jugar de nuevo? \n 1. Sí \n 2. No "))
-    switch (elijeJuego) {
-        case 1:
-            elijeJuego = parseInt(prompt("Sobre qué tematica queres jugar? Ingresá el nro. \n 1. UCM \n 2. X-men \n 3. TODO"))
-            elegirJuego()
-            break;
-            // si el usuario ingresa 2, acá si se rompe.
-        case 2:
-            break;
-    }
-}
-
-// Muestra la puntuacion del usuario
-function alertScore() {
-    alert(`Has acertado ${contadorAcertadas}, y desacertado ${contadorDesacertadas}.`)
-}
-
-// Esta funcion es de callback, por que solo se ejecuta dentro de las funciones de Preguntas.
-//Suma los puntos de datoUsuario
-function contadorDePuntos() {
-    switch (datoUsuario) {
-        case 1:
-            contadorAcertadas++
-            break;
-        case 2:
-            contadorDesacertadas++
-            break;
-        case 3:
-            contadorDesacertadas++
-            break;
-        case 4:
-            contadorDesacertadas++
-            break;
-        case 0:
-            break;
-        default:
-            break;
-    }
-}
-// Funcion de callback
-function preguntasUCM() {
-
-    datoUsuario = parseInt(prompt("Que raza es aliada de Loki en Avengers? \n Ingresá el nro correspondiente. \n 1. Chitauri \n 2. Klyntar \n 3. Kronans \n 0. Para salir"))
-    contadorDePuntos()
-    datoUsuario = parseInt(prompt("¿Por quien consigue Clint la gema del Alma? \n Ingresá el nro correspondiente. \n 1. Viuda Negra \n 2. Bruja Escarlata \n 3. Gamora \n 4. Carol Danvers \n 0. Para salir"))
-    contadorDePuntos()
-    jugarDeNuevo()
-
-}
-// Funcion de callback
-function preguntasXMEN() {
-
-    datoUsuario = parseInt(prompt("¿Qué personas tiene un premio por intrepretar en live action más veces a un superhéroe de marvel? \n 1. Hugh JackMan & Patrick Stewart \n 2. Hugh JackMan & Robert D. Junior\n 3. Patrick Stewart & Robert D. Junior \n 0. Para salir"))
-    contadorDePuntos()
-    datoUsuario = parseInt(prompt("¿En que pelicula Wolverine viaja al pasado?. \n Ingresá el numero correspondiente. \n 1. X-men Dias del Futuro Pasado \n 2. X-men 0rigenes \n 3. X-men 2 \n 0. Para salir"))
-    contadorDePuntos()
-    datoUsuario = parseInt(prompt("¿Como se llama la nave de los X-men? \n Ingresá el numero correspondiente. \n 1. X-jet \n 2. X-ship \n 3. X-moon"))
-    contadorDePuntos()
-    datoUsuario = parseInt(prompt("¿Que accidente tiene Charles Xavier que lo deja invalido? \n Ingresá el numero correspondiente. \n 1. Tiro en la espina dorsal \n 2. Accidente de auto \n 3. Golpe en la cabeza"))
-    contadorDePuntos()
-    datoUsuario = parseInt(prompt("¿En donde ocurre 'La Batalla Final' donde Logan tiene que matar a Phoenix? \n Ingresá el numero correspondiente.\n 1. Alcatraz \n 2. Genosha \n 3. Canada "))
-    contadorDePuntos()
-    datoUsuario = parseInt(prompt("¿Donde nació Lobezno? \n Ingresá el numero correspondiente.\n 1. Canada \n 2. EEUU \n 3. Inglaterra "))
-    contadorDePuntos()
-    datoUsuario = parseInt(prompt("¿Cual es el verdadero nombre de Logan? \n Ingresá el numero correspondiente.\n 1. Shimmy \n 2. James \n 3. David"))
-    contadorDePuntos()
-    jugarDeNuevo()
-}
-
-
-// function todo() {
-//     preguntasXMEN()
-//     preguntasUCM()
-// }
-
-// todo()
