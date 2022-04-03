cc2a6c3fff34aed2472d391963a12446bd02a72d
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-04-03 11:30:04 -0300

    Funciona. Pero no toma el 0 para salir

--- js/quizz.js
+++ js/quizz.js
@@ -15,23 +15,13 @@ class Pregunta {
         this.datoDelUsuario = parseInt(prompt(this.pregunta + this.opciones))
     }
     contadorDePuntos() {
-        switch (this.datoDelUsuario) {
-            case 1:
-                contadorAcertadas++
-                break;
-            case 2:
-                contadorDesacertadas++
-                break;
-            case 3:
-                contadorDesacertadas++
-                break;
-            case 4:
-                contadorDesacertadas++
-                break;
-            case 0:
-                break;
-            default:
-                break;
+
+        if (this.datoDelUsuario != 1 && this.datoDelUsuario != 0) {
+            contadorDesacertadas++
+        } else if (this.datoDelUsuario == 0) {
+            console.log("break");
+        } else {
+            contadorAcertadas++
         }
     }
     alertScore() {
@@ -66,7 +56,12 @@ function preguntasUCM() {
     preguntaUCM2.mostrarPregunta()
     preguntaUCM2.contadorDePuntos()
 
-    preguntaUCM2.jugarDeNuevo()
+    let preguntaUCM3 = new Pregunta("¿Que Avenger crea a Vision? \n Ingresá el nro correspondiente.",
+        "\n 1. Iron Man \n 2. Thor \n 3. Bruja Escarlata \n 4. Clint \n 0. Para salir", datoUsuario)
+    preguntaUCM3.mostrarPregunta()
+    preguntaUCM3.contadorDePuntos()
+
+    preguntaUCM3.jugarDeNuevo()
 }
 
 function preguntasXMEN() {
@@ -109,7 +104,58 @@ function preguntasXMEN() {
     preguntaXMEN7.jugarDeNuevo()
 }
 
-let elijeJuego = parseInt(prompt("Sobre qué tematica queres jugar? Ingresá el nro. \n 1. UCM \n 2. X-men"))
+function preguntasTodo() {
+
+    let preguntaTodo1 = new Pregunta("¿Qué personas tiene un premio por intrepretar en live action más veces a un superhéroe de marvel? \n Ingresá el numero correspondiente.",
+        "\n 1. Hugh JackMan & Patrick Stewart \n 2. Hugh JackMan & Robert D. Junior\n 3. Patrick Stewart & Robert D. Junior \n 0. Para salir", datoUsuario)
+    preguntaTodo1.mostrarPregunta()
+    preguntaTodo1.contadorDePuntos()
+
+    let preguntaTodo2 = new Pregunta("¿En que pelicula Wolverine viaja al pasado? \n Ingresá el nro correspondiente.",
+        "\n 1. X-men Dias del Futuro Pasado \n 2. X-men 0rigenes \n 3. X-men 2 \n 0. Para salir", datoUsuario)
+    preguntaTodo2.mostrarPregunta()
+    preguntaTodo2.contadorDePuntos()
+
+    let preguntaTodo3 = new Pregunta("¿Como se llama la nave de los X-men? \n Ingresá el nro correspondiente.",
+        "\n 1. X-jet \n 2. X-ship \n 3. X-moon", datoUsuario)
+    preguntaTodo3.mostrarPregunta()
+    preguntaTodo3.contadorDePuntos()
+
+    let preguntaTodo4 = new Pregunta("¿Que accidente tiene Charles Xavier que lo deja invalido? \n Ingresá el nro correspondiente.",
+        "\n 1. Tiro en la espina dorsal \n 2. Accidente de auto \n 3. Golpe en la cabeza", datoUsuario)
+    preguntaTodo4.mostrarPregunta()
+    preguntaTodo4.contadorDePuntos()
+
+
+    let preguntaTodo5 = new Pregunta("¿En donde ocurre 'La Batalla Final' donde Logan tiene que matar a Phoenix? \n Ingresá el nro correspondiente.",
+        "\n 1. Alcatraz \n 2. Genosha \n 3. Canada", datoUsuario)
+    preguntaTodo5.mostrarPregunta()
+    preguntaTodo5.contadorDePuntos()
+
+    let preguntaTodo6 = new Pregunta("¿Donde nació Lobezno? \n Ingresá el nro correspondiente.",
+        "\n 1. Canada \n 2. EEUU \n 3. Inglaterra", datoUsuario)
+    preguntaTodo6.mostrarPregunta()
+    preguntaTodo6.contadorDePuntos()
+
+    let preguntaTodo7 = new Pregunta("¿Cual es el verdadero nombre de Logan? \n Ingresá el nro correspondiente.",
+        "\n 1. Shimmy \n 2. James \n 3. David", datoUsuario)
+    preguntaTodo7.mostrarPregunta()
+    preguntaTodo7.contadorDePuntos()
+
+    let preguntaTodo8 = new Pregunta("Que raza es aliada de Loki en Avengers? Ingresá el numero correspondiente.",
+        "\n1. Chitauri \n 2. Klyntar \n 3. Kronans \n 0. Para salir", datoUsuario)
+    preguntaTodo8.mostrarPregunta()
+    preguntaTodo8.contadorDePuntos()
+
+    let preguntaTodo9 = new Pregunta("¿Por quien consigue Clint la gema del Alma? \n Ingresá el nro correspondiente.",
+        "\n 1. Viuda Negra \n 2. Bruja Escarlata \n 3. Gamora \n 4. Carol Danvers \n 0. Para salir", datoUsuario)
+    preguntaTodo9.mostrarPregunta()
+    preguntaTodo9.contadorDePuntos()
+
+
+    preguntaTodo9.jugarDeNuevo()
+}
+let elijeJuego = parseInt(prompt("Sobre qué tematica queres jugar? Ingresá el nro. \n 1. UCM \n 2. X-men \n 3. TODO"))
 
 function elegirJuego() {
     switch (elijeJuego) {
@@ -120,9 +166,8 @@ function elegirJuego() {
             preguntasXMEN()
             break;
         case 3:
-            todo()
+            preguntasTodo()
             break;
-            // El caso 0, no me funciona como el caso 2 de jugarDeNuevo(). No se rompe si el usuario ingresa 0.
         default:
             break;
     }
