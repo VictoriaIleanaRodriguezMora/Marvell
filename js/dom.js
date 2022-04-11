bbb27f87db8cb352a98e237ffe9271164585c9b4
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-04-10 22:37:07 -0300

    Primeros avances con el DOM

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,30 @@
 let botonJugas = document.querySelector(".jugas")
-botonJugas.addEventListener("click", fadeOut)
+let titulo = document.querySelector("#titulo")
+let secccionQuizz = document.querySelector(".quizz")
+let divCategorias = document.querySelector(".categorias")
+let divPreguntas = document.querySelector(".contenedor__preguntas")
+let botonesCategorias = document.querySelectorAll(".categorias > button")
 
-function fadeOut() {
-    botonJugas.style.transition = "300ms"
-    botonJugas.style.opacity = "0"
-    botonJugas.style.display = "hidden"
+// Se cliake el boton ¿JUGAS?
+botonJugas.addEventListener("click", fadeOutInicial)
+    //Se oculta ese boton, y la presentacion del Inicio
+function fadeOutInicial() {
+    botonJugas.classList.add("fadeOut")
+    titulo.classList.add("fadeOut")
+        // Se muestran las categorias para jugar
+    divCategorias.classList.add("fadeShow")
 }
+
+//Se muestran los 3 botones para elegir
+function showCategorias() {
+    for (let i = 0; i < botonesCategorias.length; i++) {
+        botonesCategorias[i].addEventListener("click", function() {
+            this.classList.remove("categorias__boton")
+            this.classList.add("buttonSelected")
+            botonesCategorias.classList.remove("categorias__boton")
+            botonesCategorias.classList.add("fadeOut")
+        })
+    }
+    divPreguntas.classList.add("fadeShow")
+}
+showCategorias()
