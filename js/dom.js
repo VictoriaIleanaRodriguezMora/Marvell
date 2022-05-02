8aeb5dba2a88f7f73e5ee158f7d95b8c256d62d5
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-05-01 23:13:22 -0300

    Organizacion del codigo. Avance en que la fecha se muestre en span

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,8 @@
-//LUXON
-const DateTime = luxon.DateTime
-let fechaDeJuego = DateTime.now()
-let mnsFechaDeJuego = (`Ultima vez el ${fechaDeJuego.toLocaleString()} a las ${fechaDeJuego.toLocaleString(DateTime.TIME_SIMPLE)}`)
-//LUXON
-
 //HTML
 let titulo = document.querySelector("#titulo")
 let botonJugas = document.querySelector(".jugas")
 let botonJugarDeNuevo = document.querySelector(".jugarDeNuevo")
 let score = document.querySelector("#score")
-
 //Variables contenedoras
 let contenedorDeTodo = document.querySelector(".contenedor__preguntas__general")
 let divCategoriasDeJuego = document.querySelectorAll(".categorias__boton")
@@ -22,7 +15,6 @@ let preguntasUCM = document.querySelectorAll(".ucm")
 let preguntasXMEN = document.querySelectorAll(".xmen")
 let preguntasTODO = document.querySelectorAll(".todo")
 let arrPregs = [preguntasUCM, preguntasXMEN, preguntasTODO]
-//
 //Opciones
 let opcionesUCM = document.querySelectorAll(".ucm > .pregunta__opcion")
 let opcionesXMEN = document.querySelectorAll(".xmen > .pregunta__opcion")
@@ -35,22 +27,34 @@ let botonesCategoriasDeJuego = document.querySelectorAll(".categorias > button")
 let botonUCM = document.querySelector("#botonUCM")
 let botonXMEN = document.querySelector("#botonXMEN")
 let botonTODO = document.querySelector("#botonTODO")
+//Ranking
 let divRanking = document.querySelector("#ranking")
 let botonRanking = document.querySelector(".ranking")
 let divRankingP = document.querySelectorAll("#ranking__p")
 let rankingP = document.querySelectorAll(".ranking__p")
+let spanRanking = document.querySelectorAll(".ranking__span")
+
 //Contadores
 let preguntasTotales = 0;
 let contadorAcertadas = 0;
 let contadorDesacertadas = 0;
 let limiteOpciones = 0;
 
+//LUXON
+const DateTime = luxon.DateTime
+let fechaDeJuego = DateTime.now()
+let mnsFechaDeJuego = (`Ultima vez el ${fechaDeJuego.toLocaleString()} a las ${fechaDeJuego.toLocaleString(DateTime.TIME_SIMPLE)}`)
+//LUXON
+
 // Terminan las variables
 //Oculto lo que necesito para empezar
 function fadeOut(array) {
     for (let i = 0; i < array.length; i++) {
-        array[i].classList.toggle("fadeOut")
+        array[i].classList.add("fadeOut")
     }
+}
+function fadeOutSingular(element) {
+    element.classList.toggle("fadeOut")
 }
 function fadeOutRemove(array) {
     for (let i = 0; i < array.length; i++) {
@@ -63,9 +67,10 @@ fadeOut(preguntasUCM)
 fadeOut(preguntasXMEN)
 fadeOut(preguntasTODO)
 fadeOut(divRankingP)
-botonJugarDeNuevo.classList.add("fadeOut")
-score.classList.add("fadeOut")
-botonTerminar.classList.add("fadeOut")
+fadeOut(spanRanking)
+fadeOutSingular(botonJugarDeNuevo)
+fadeOutSingular(score)
+fadeOutSingular(botonTerminar)
 contenedorDeTodo.classList.remove("contenedor__preguntas__general")
 botonJugarDeNuevo.removeAttribute("href", "#header")
 
@@ -83,6 +88,7 @@ function fadeOutInicial() {
     }
 
 }
+//Funcion que se ejecuta en todos los botones de categorias
 function botonesCat(botonPrincipal, segundoBoton, tercerBoton, contenedorUno, contenedorDos, botonFinish) {
     contenedorDeTodo.classList.add("contenedor__preguntas__general")
     botonPrincipal.classList.remove("categorias__boton")
@@ -95,9 +101,8 @@ function botonesCat(botonPrincipal, segundoBoton, tercerBoton, contenedorUno, co
     contenedorDos.classList.remove("contenedores")
     divRanking.classList.add("fadeOut")
 }
-//Elegir Juego
 
-//Almacenar Info
+//Almacenar Info en Local Storage
 function almacenarInfo(vecesJugadas) {
     let dataJuego = JSON.parse(localStorage.getItem(vecesJugadas))
     let newData;
@@ -112,61 +117,71 @@ function almacenarInfo(vecesJugadas) {
     return newData;
 
 }
-//UCM
+//Elegir Juego
+
+//BOTON UCM
 botonUCM.onclick = () => {
     almacenarInfo("vecesJugadasUCM")
     botonesCat(botonUCM, botonXMEN, botonTODO, contPregsXMEN, contPregsTODO, botonTerminar)
     recorrerInicial(arrContPregs, 0, preguntasUCM, 0)
     jugar(preguntasUCM)
-    localStorage.setItem("ultVezJugadaUCM", mnsFechaDeJuego)
+    localStorage.setItem("ultVezJugadaUCM", spanRanking)
+
 }
-//XMEN
+//BOTON XMEN
 botonXMEN.onclick = () => {
     almacenarInfo("vecesJugadasXMEN")
     botonesCat(botonXMEN, botonUCM, botonTODO, contPregsUCM, contPregsTODO, botonTerminar)
     recorrerInicial(arrContPregs, 1, preguntasXMEN)
     jugar(preguntasXMEN)
     localStorage.getItem("vecesJugadasXMEN");
-    localStorage.setItem("ultVezJugadaXMEN", mnsFechaDeJuego)
+    localStorage.setItem("ultVezJugadaXMEN", spanRanking)
+
 
 }
-//TODI
+//BOTON TODO
 botonTODO.onclick = () => {
     almacenarInfo("vecesJugadasTODO")
     botonesCat(botonTODO, botonUCM, botonXMEN, contPregsUCM, contPregsXMEN, botonTerminar)
     recorrerInicial(arrContPregs, 2, preguntasTODO)
     jugar(preguntasTODO)
     localStorage.getItem("vecesJugadasTODO");
-    localStorage.setItem("ultVezJugadaTODO", mnsFechaDeJuego)
+    localStorage.setItem("ultVezJugadaTODO", spanRanking)
+
 
 }
-//RANKING
+//BOTON RANKING
 botonRanking.onclick = () => {
     botonRanking.classList.add("fadeOut")
     fadeOutRemove(divRankingP)
+    fadeOutRemove(spanRanking)
     for (let i = 0; i < rankingP.length; i++) {
-
-        function ranking(vecesJugadas, poscion, boton, ultVezJugada) {
+        function ranking(vecesJugadas, posc, boton, ultVezJugada) {
             if (localStorage.getItem(vecesJugadas) === null) {
-                rankingP[poscion].innerText = (`Aun no has jugado a ${boton}`)
+                rankingP[posc].innerText = (`Aun no has jugado a ${boton}`)
             } else if (localStorage.getItem(vecesJugadas) == 1) {
-                rankingP[poscion].innerText = (`Has jugado a ${boton} ${localStorage.getItem(vecesJugadas)} vez`)
+                rankingP[posc].innerText = (`Has jugado a ${boton} ${localStorage.getItem(vecesJugadas)} vez`)
+                spanRanking[posc].innerText = localStorage.getItem(ultVezJugada)
+
             }
             else {
-                rankingP[poscion].innerText = (`Has jugado a ${boton} ${localStorage.getItem(vecesJugadas)} veces. ${localStorage.getItem(ultVezJugada)}`)
+                rankingP[posc].innerText = (`Has jugado a ${boton} ${localStorage.getItem(vecesJugadas)} veces.`)
+                spanRanking[posc].innerText = localStorage.getItem(ultVezJugada)
             }
+
         }
         ranking("vecesJugadasUCM", 0, botonUCM.innerText, "ultVezJugadaUCM")
         ranking("vecesJugadasXMEN", 1, botonXMEN.innerText, "ultVezJugadaXMEN")
         ranking("vecesJugadasTODO", 2, botonTODO.innerText, "ultVezJugadaTODO")
     }
 }
-//Funcion para automatizar 
 function recorrerInicial(arrGeneral, n, arrPregunta) {
+//Funcion para automatizar 
     arrGeneral[n].classList.remove("fadeOut")
     fadeOutRemove(arrPregunta)
 
 }
+//Evalua que boton se apretó
 function contarPuntos() {
     for (let i = 0; i < opcionesIncorrectas.length; i++) {
         opcionesIncorrectas[i].addEventListener("click", function () {
@@ -189,6 +204,7 @@ function contarPuntos() {
     }
 
 }
+//El parametro se ejecuta en cada boton. Suma cantidad de preguntas y evalua puntos
 function jugar(arr) {
     for (let i = 0; i < arr.length; i++) {
         preguntasTotales++
@@ -196,45 +212,38 @@ function jugar(arr) {
 
     contarPuntos()
 }
+//Termina el juego
 botonTerminar.onclick = () => {
-    // this.style.disabled = true
-
-    botonTerminar.classList.add("fadeOut")
+    fadeOutSingular(botonTerminar)
     //Muestro el puntaje
     alertScore()
     //Reinicio los contadores
     contadorAcertadas = 0
     contadorDesacertadas = 0
     preguntasTotales = 0
-    botonJugarDeNuevo.classList.remove("fadeOut")
+    fadeOutSingular(botonJugarDeNuevo)
 
 }
+//Te reubica al principio
 botonJugarDeNuevo.onclick = () => {
-    //location.reload()
     botonJugarDeNuevo.setAttribute("href", "#header")
-    console.log(localStorage.getItem("vecesJugadasUCM"));
 }
 
-
-
-
+//Calcula los puntos 
 function alertScore() {
     score.classList.remove("fadeOut")
     //Estos son cuentas para calcular el porcentaje de respuestas correctas e incorrectas. No hay problemas con esta funcion. Se ejecuta en jugarDeNuevo
-    contadorAcertadas < preguntasTotales * 10 / 100 
-    ? 
-    score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Creo que vos no viste ni una pelicula. 🥴`)  :
-     contadorAcertadas < preguntasTotales * 30 / 100 
-     ?
-    score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}.`) :
-    contadorAcertadas < preguntasTotales * 60 / 100 
-    ? 
-    score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bien! Seguro que podes hacerlo mejor 😉`) : 
-    contadorAcertadas < preguntasTotales * 100 / 100 
-    ? 
-    score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bueno, si fuera Fury, te habria considerado! 😁`) :
-    contadorAcertadas == preguntasTotales * 100 / 100 ?
-    score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Sos un expert@! 🥳`) : console.log("");
+    if (contadorAcertadas < preguntasTotales * 10 / 100) {
+        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Creo que vos no viste ni una pelicula. 🥴`)
+    } else if (contadorAcertadas < preguntasTotales * 30 / 100) {
+        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bueno, nadie es perfecto... 😵`)
+    } else if (contadorAcertadas < preguntasTotales * 60 / 100) {
+        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bien! Seguro que podes hacerlo mejor 😉`)
+    } else if (contadorAcertadas < preguntasTotales * 100 / 100) {
+        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bueno, si fuera Fury, te habria considerado! 😁`)
+    } else if (contadorAcertadas == preguntasTotales * 100 / 100) {
+        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Sos un expert@! 🥳`)
+    }
 }
 
 
