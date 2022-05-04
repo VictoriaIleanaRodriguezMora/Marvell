fd22d9d628f7ee67fdc3930275f1c5856f6a901c
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2022-05-04 16:32:21 -0300

    Conecté con la API DE MARVEL

--- js/dom.js
+++ js/dom.js
@@ -1,108 +1,75 @@
-//HTML
-let titulo = document.querySelector("#titulo")
-let botonJugas = document.querySelector(".jugas")
-let botonJugarDeNuevo = document.querySelector(".jugarDeNuevo")
-let score = document.querySelector("#score")
+let titulo = document.querySelector('#titulo')
+let botonJugas = document.querySelector('#jugas')
+let botonJugarDeNuevo = document.querySelector('.jugarDeNuevo')
+let score = document.querySelector('#score')
+let btnSiguiente = document.querySelector('.terminar')
 //Variables contenedoras
-let contenedorDeTodo = document.querySelector(".contenedor__preguntas__general")
-let divCategoriasDeJuego = document.querySelectorAll(".categorias__boton")
-let contPregsUCM = document.querySelector(".contenedor__preguntas__UCM")
-let contPregsXMEN = document.querySelector(".contenedor__preguntas__XMEN")
-let contPregsTODO = document.querySelector(".contenedor__preguntas__TODO")
-let arrContPregs = [contPregsUCM, contPregsXMEN, contPregsTODO]
-// Div Preguntas
-let preguntasUCM = document.querySelectorAll(".ucm")
-let preguntasXMEN = document.querySelectorAll(".xmen")
-let preguntasTODO = document.querySelectorAll(".todo")
-let arrPregs = [preguntasUCM, preguntasXMEN, preguntasTODO]
-//Opciones
-let opcionesUCM = document.querySelectorAll(".ucm > .pregunta__opcion")
-let opcionesXMEN = document.querySelectorAll(".xmen > .pregunta__opcion")
-let opcionesTODO = document.querySelectorAll(".todo > .pregunta__opcion")
-let opcionesCorrectas = document.querySelectorAll(".opcion__correcta")
-let opcionesIncorrectas = document.querySelectorAll("#opIncorrecta")
-//Botones de juego
-let botonTerminar = document.querySelector(".terminar")
-let botonesCategoriasDeJuego = document.querySelectorAll(".categorias > button")
-let botonUCM = document.querySelector("#botonUCM")
-let botonXMEN = document.querySelector("#botonXMEN")
-let botonTODO = document.querySelector("#botonTODO")
+let contenedorDeTodo = document.querySelector('.contenedor__preguntas__general')
+let divCategoriasDeJuego = document.querySelectorAll('.categorias__boton')
+let divPregunta = document.querySelector('.div__pregunta')
+let arrbotonesPregs = document.querySelector('.botonesPregs')
 //Ranking
-let divRanking = document.querySelector("#ranking")
-let botonRanking = document.querySelector(".ranking")
-let divRankingP = document.querySelectorAll("#ranking__p")
-let rankingP = document.querySelectorAll(".ranking__p")
+let divRanking = document.querySelector('#ranking')
+let botonRanking = document.querySelector('.ranking')
+let divRankingP = document.querySelector('#ranking__p')
+let rankingP = document.querySelectorAll('.ranking__p')
 let spanRanking = document.querySelectorAll(".ranking__span")
 
-//Contadores
-let preguntasTotales = 0;
-let contadorAcertadas = 0;
-let contadorDesacertadas = 0;
-let limiteOpciones = 0;
 
+//Opciones
+let pregunta = document.querySelector('.pregunta__titulo')
+//Botones de juego
+let botonUCM = document.querySelector('#botonUCM')
+let botonXMEN = document.querySelector('#botonXMEN')
+let botonTODO = document.querySelector('#botonTODO')
+//Contadores
+let preguntasTotales = 0
+let contadorAcertadas = 0
+let contadorDesacertadas = 0
+let limiteOpciones = 0
+let vecesJugadasUCM = 0
+let vecesJugadasXMEN = 0
+let vecesJugadasTODO = 0
+let vecesJugadasUCMLS
+
+let arrDisplayNone = [botonJugarDeNuevo, score, btnSiguiente, divRankingP]
+//
+let preguntasDesordenadas = 0
+let posicionPregs = 0
+let arrBtn = []
+let btn
 //LUXON
 const DateTime = luxon.DateTime
 let fechaDeJuego = DateTime.now().toLocaleString()
 let hsDeJuego = DateTime.now().toLocaleString(DateTime.TIME_SIMPLE)
-// let mnsFechaDeJuego = (`Ultima vez el ${fechaDeJuego.toLocaleString()} a las ${fechaDeJuego.toLocaleString(DateTime.TIME_SIMPLE)}`)
+
 //LUXON
-
 // Terminan las variables
-//Oculto lo que necesito para empezar
-function fadeOut(array) {
+function displayNone(array) {
     for (let i = 0; i < array.length; i++) {
-        array[i].classList.add("fadeOut")
+        array[i].classList.add('displayNone')
     }
 }
-function fadeOutSingular(element) {
-    element.classList.toggle("fadeOut")
-}
-function fadeOutRemove(array) {
+
+function removeDisplayNone(array) {
     for (let i = 0; i < array.length; i++) {
-        array[i].classList.remove("fadeOut")
+        array[i].classList.remove('displayNone')
     }
 }
-fadeOut(divCategoriasDeJuego)
-fadeOut(arrContPregs)
-fadeOut(preguntasUCM)
-fadeOut(preguntasXMEN)
-fadeOut(preguntasTODO)
-fadeOut(divRankingP)
-fadeOut(spanRanking)
-fadeOutSingular(botonJugarDeNuevo)
-fadeOutSingular(score)
-fadeOutSingular(botonTerminar)
-contenedorDeTodo.classList.remove("contenedor__preguntas__general")
-botonJugarDeNuevo.removeAttribute("href", "#header")
+displayNone(rankingP)
+// Configuraciones iniciales
+displayNone(arrDisplayNone)
+displayNone(divCategoriasDeJuego)
+displayNone(btnSiguiente)
+divRankingP.classList.add('displayNone')
+divPregunta.classList.add('displayNone')
 
-// Empieza el juego
-// Se clickea el boton ¿JUGAS?
-botonJugas.addEventListener("click", fadeOutInicial)
-function fadeOutInicial() {
-    //Se oculta ese boton, y la presentacion del Inicio
-    botonJugas.classList.toggle("fadeOut")
-    titulo.classList.toggle("fadeOut")
-    // Se muestran las categorias para jugar
-    for (let i = 0; i < divCategoriasDeJuego.length; i++) {
-        //Mostrar categorias y elegir juego
-        divCategoriasDeJuego[i].classList.toggle("fadeOut")
-    }
-
+//Inicia juego
+botonJugas.onclick = () => {
+    botonJugas.classList.add('displayNone')
+    titulo.classList.add('displayNone')
+    removeDisplayNone(divCategoriasDeJuego)
 }
-//Funcion que se ejecuta en todos los botones de categorias
-function botonesCat(botonPrincipal, segundoBoton, tercerBoton, contenedorUno, contenedorDos, botonFinish) {
-    contenedorDeTodo.classList.add("contenedor__preguntas__general")
-    botonPrincipal.classList.remove("categorias__boton")
-    botonPrincipal.classList.add("buttonSelected")
-    botonPrincipal.disabled = true
-    segundoBoton.classList.add('fadeOut');
-    tercerBoton.classList.add('fadeOut');
-    botonFinish.classList.remove("fadeOut")
-    contenedorUno.classList.remove("contenedores")
-    contenedorDos.classList.remove("contenedores")
-    divRanking.classList.add("fadeOut")
-}
-
 //Almacenar Info en Local Storage
 function almacenarInfo(vecesJugadas) {
     let dataJuego = JSON.parse(localStorage.getItem(vecesJugadas))
@@ -118,49 +85,102 @@ function almacenarInfo(vecesJugadas) {
     return newData;
 
 }
-//Elegir Juego
 
-//BOTON UCM
+//Funcion que se ejecuta en todos los botones de categoria
+function botonesCategorias(botonPrincipal, botonSecundario, botonSecundario2, arrPreguntas) {
+    botonPrincipal.style.disabled = 'true'
+    botonPrincipal.classList.remove('categorias__boton')
+    botonPrincipal.classList.add('btnSelected')
+    botonSecundario.classList.add('displayNone')
+    botonSecundario2.classList.add('displayNone')
+    divRanking.classList.add('displayNone')
+    desordenarPreguntas(arrPreguntas)
+    rellenarPregunta(preguntasDesordenadas[posicionPregs])
+
+}
 botonUCM.onclick = () => {
     almacenarInfo("vecesJugadasUCM")
-    botonesCat(botonUCM, botonXMEN, botonTODO, contPregsXMEN, contPregsTODO, botonTerminar)
-    recorrerInicial(arrContPregs, 0, preguntasUCM, 0)
-    jugar(preguntasUCM)
+    botonesCategorias(botonUCM, botonXMEN, botonTODO, preguntasUCM)
     localStorage.getItem("vecesJugadasUCM");
     localStorage.setItem("ultVezJugadaUCM", fechaDeJuego)
     localStorage.setItem("hsDeJuegoUCM", hsDeJuego)
-
 }
-//BOTON XMEN
 botonXMEN.onclick = () => {
-    almacenarInfo("vecesJugadasXMEN")
-    botonesCat(botonXMEN, botonUCM, botonTODO, contPregsUCM, contPregsTODO, botonTerminar)
-    recorrerInicial(arrContPregs, 1, preguntasXMEN)
-    jugar(preguntasXMEN)
-
+    botonesCategorias(botonXMEN, botonUCM, botonTODO, preguntasXMEN)
     localStorage.getItem("vecesJugadasXMEN");
     localStorage.setItem("ultVezJugadaXMEN", fechaDeJuego)
     localStorage.setItem("hsDeJuegoXMEN", hsDeJuego)
-
-
 }
-//BOTON TODO
 botonTODO.onclick = () => {
-    almacenarInfo("vecesJugadasTODO")
-    botonesCat(botonTODO, botonUCM, botonXMEN, contPregsUCM, contPregsXMEN, botonTerminar)
-    recorrerInicial(arrContPregs, 2, preguntasTODO)
-    jugar(preguntasTODO)
-
+    botonesCategorias(botonTODO, botonUCM, botonXMEN, preguntasTODO)
     localStorage.getItem("vecesJugadasTODO");
     localStorage.setItem("ultVezJugadaTODO", fechaDeJuego)
     localStorage.setItem("hsDeJuegoTODO", hsDeJuego)
-
 }
+function desordenarPreguntas(array) {
+    preguntasDesordenadas = array.sort(() => Math.random() - 0.5)
+}
+function rellenarPregunta(arr) {
+    if (arr === undefined) {
+        terminaElJuego()
+    } else {
+        divPregunta.classList.remove("displayNone")
+        preguntasTotales++
+        console.log(`preguntas totales ${preguntasTotales}`)
+        pregunta.innerText = arr['pregunta']
+        for (let i = 0; i < arr['respuestas'].length; i++) {
+            console.log("else")
+            btn = document.createElement('button')
+            arrbotonesPregs.appendChild(btn)
+            arrBtn.push(arr['respuestas'][i]['rta'])
+            btn.classList.add('pregunta__opcion')
+            btn.innerText = arr['respuestas'][i]['rta']
+            btn.addEventListener('click', function () {
+                if (arr['respuestas'][i]['correcto'] == true) {
+                    this.classList.add("opcionCorrecta")
+                    contadorAcertadas++
+                    console.log(contadorAcertadas + 'a')
+                    btn.style.disabled = 'true'
+                    siguientePreg()
+                } else {
+                    contadorDesacertadas++
+                    console.log(contadorDesacertadas + 'des')
+                    btn.style.disabled = 'true'
+                    siguientePreg()
+                }
+            })
+
+        }
+    }
+}
+function terminaElJuego() {
+    divPregunta.classList.add("displayNone")
+    alertScore()
+    botonJugarDeNuevo.classList.remove("displayNone")
+}
+function siguientePreg() {
+    btnSiguiente.classList.remove('displayNone')
+    btnSiguiente.addEventListener('click', btnSig)
+}
+
+function btnSig() {
+    posicionPregs++
+    arrBtn = []
+    btnSiguiente.classList.add('displayNone')
+    while (arrbotonesPregs.firstChild) {
+        arrbotonesPregs.removeChild(arrbotonesPregs.firstChild)
+    }
+    rellenarPregunta(preguntasDesordenadas[posicionPregs])
+}
+
+
 //BOTON RANKING
 botonRanking.onclick = () => {
-    botonRanking.classList.add("fadeOut")
-    fadeOutRemove(divRankingP)
-    fadeOutRemove(spanRanking)
+    botonRanking.classList.add("displayNone")
+    divRankingP.classList.remove("displayNone")
+    removeDisplayNone(rankingP)
+    removeDisplayNone(spanRanking)
+
     for (let i = 0; i < rankingP.length; i++) {
         function ranking(vecesJugadas, posc, boton, ultVezJugada, hsDeJuego) {
             if (localStorage.getItem(vecesJugadas) === null) {
@@ -181,73 +201,85 @@ botonRanking.onclick = () => {
         ranking("vecesJugadasTODO", 2, botonTODO.innerText, "ultVezJugadaTODO", "hsDeJuegoTODO")
     }
 }
-function recorrerInicial(arrGeneral, n, arrPregunta) {
-    //Funcion para automatizar 
-    arrGeneral[n].classList.remove("fadeOut")
-    fadeOutRemove(arrPregunta)
-
+divRankingP.onclick = () => {
+    botonRanking.classList.remove("displayNone")
+    divRankingP.classList.add("displayNone")
 }
-//Evalua que boton se apretó
-function contarPuntos() {
-    for (let i = 0; i < opcionesIncorrectas.length; i++) {
-        opcionesIncorrectas[i].addEventListener("click", function () {
-            contadorDesacertadas++
-            limiteOpciones++
-            opcionesIncorrectas.disabled = true
-            opcionesCorrectas.disabled = true
-            opcionesIncorrectas[i].classList.add("opcionSeleccionada")
-        })
-
-    }
-    for (let x = 0; x < opcionesCorrectas.length; x++) {
-        opcionesCorrectas[x].addEventListener("click", function () {
-            contadorAcertadas++
-            limiteOpciones++
-            opcionesCorrectas.disabled = true
-            opcionesIncorrectas.disabled = true
-            opcionesCorrectas[x].classList.add("opcionSeleccionada")
-        })
-    }
-
-}
-//El parametro se ejecuta en cada boton. Suma cantidad de preguntas y evalua puntos
-function jugar(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        preguntasTotales++
-    }
-    contarPuntos()
-}
-//Termina el juego
-botonTerminar.onclick = () => {
-    fadeOutSingular(botonTerminar)
-    //Muestro el puntaje
-    alertScore()
-    //Reinicio los contadores
-    contadorAcertadas = 0
-    contadorDesacertadas = 0
-    preguntasTotales = 0
-    fadeOutSingular(botonJugarDeNuevo)
-
-}
-//Te reubica al principio
-botonJugarDeNuevo.onclick = () => {
-    botonJugarDeNuevo.setAttribute("href", "#header")
-}
-
-//Calcula los puntos 
 function alertScore() {
-    score.classList.remove("fadeOut")
+    score.classList.remove('displayNone')
     //Estos son cuentas para calcular el porcentaje de respuestas correctas e incorrectas. No hay problemas con esta funcion. Se ejecuta en jugarDeNuevo
-    if (contadorAcertadas < preguntasTotales * 10 / 100) {
-        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Creo que vos no viste ni una pelicula. 🥴`)
-    } else if (contadorAcertadas < preguntasTotales * 30 / 100) {
-        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bueno, nadie es perfecto... 😵`)
-    } else if (contadorAcertadas < preguntasTotales * 60 / 100) {
-        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bien! Seguro que podes hacerlo mejor 😉`)
-    } else if (contadorAcertadas < preguntasTotales * 100 / 100) {
-        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bueno, si fuera Fury, te habria considerado! 😁`)
-    } else if (contadorAcertadas == preguntasTotales * 100 / 100) {
-        score.innerText = (`Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Sos un expert@! 🥳`)
+    if (contadorAcertadas < (preguntasTotales * 10) / 100) {
+        score.innerText = `Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Creo que vos no viste ni una pelicula. 🥴`
+    } else if (contadorAcertadas < (preguntasTotales * 30) / 100) {
+        score.innerText = `Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bueno, nadie es perfecto... 😵`
+    } else if (contadorAcertadas < (preguntasTotales * 60) / 100) {
+        score.innerText = `Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bien! Seguro que podes hacerlo mejor 😉`
+    } else if (contadorAcertadas < (preguntasTotales * 100) / 100) {
+        score.innerText = `Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Bueno, si fuera Fury, te habria considerado! 😁`
+    } else if (contadorAcertadas == (preguntasTotales * 100) / 100) {
+        score.innerText = `Has acertado ${contadorAcertadas}, de ${preguntasTotales}. Sos un expert@! 🥳`
     }
 }
- 
+
+const preguntasUCM = [
+    {
+        pregunta: '¿Por quien consigue Clint la gema del Alma?',
+        respuestas: [
+            { rta: 'Bruja Escarlata', correcto: false },
+            { rta: 'Gamora', correcto: false },
+            { rta: 'Viuda Negra', correcto: true },
+            { rta: 'Carol Danvers', correcto: false },
+        ],
+    },
+    {
+        pregunta: '¿Que raza es aliada de Loki en Avengers??',
+        respuestas: [
+            { rta: 'Chitauri', correcto: true },
+            { rta: 'Klyntar', correcto: false },
+            { rta: 'Kronans', correcto: false },
+            { rta: 'Centaurians', correcto: false },
+        ],
+    },
+]
+
+const preguntasXMEN = [
+    {
+        pregunta: '¿Por quien consigue Clint la gema del Alma?',
+        respuestas: [
+            { rta: 'Bruja Escarlata', correcto: false },
+            { rta: 'Gamora', correcto: false },
+            { rta: 'Viuda Negra', correcto: true },
+            { rta: 'Carol Danvers', correcto: false },
+        ],
+    },
+    {
+        pregunta: '¿Que raza es aliada de Loki en Avengers??',
+        respuestas: [
+            { rta: 'Chitauri', correcto: false },
+            { rta: 'Klyntar', correcto: false },
+            { rta: 'Kronans', correcto: true },
+            { rta: 'Centaurians', correcto: false },
+        ],
+    },
+]
+
+const preguntasTODO = [
+    {
+        pregunta: '¿Por quien consigue Clint la gema del Alma?',
+        respuestas: [
+            { rta: 'Bruja Escarlata', correcto: false },
+            { rta: 'Gamora', correcto: false },
+            { rta: 'Viuda Negra', correcto: true },
+            { rta: 'Carol Danvers', correcto: false },
+        ],
+    },
+    {
+        pregunta: '¿Que raza es aliada de Loki en Avengers??',
+        respuestas: [
+            { rta: 'Chitauri', correcto: false },
+            { rta: 'Klyntar', correcto: false },
+            { rta: 'Kronans', correcto: true },
+            { rta: 'Centaurians', correcto: false },
+        ],
+    },
+]
--- js/json.js
+++ js/json.js
@@ -1,4 +1,4 @@
-fetch('../json/basePregs.json')
+fetch('http://gateway.marvel.com/v1/public/comics?ts=1000&apikey=7e83954a5a388e9cfab7dcfce184a263&hash=c963bd5f34ea7903c1c3e8b424e65745')
 .then((respuesta) => respuesta.json())
 .then((data) => {
     console.log(data);
